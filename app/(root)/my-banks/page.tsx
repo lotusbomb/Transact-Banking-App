547df4152623053f23f2ebedc476638ba8006780
lotusbomb <chukwumafavour600@gmail.com>
2024-09-27 01:52:57 +0100

    changes in esLint

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,3 +1,5 @@
+
+
 import BankCard from "@/components/BankCard";
 import HeaderBox from "@/components/HeaderBox";
 import { getAccounts } from "@/lib/actions/bank.action";
@@ -15,20 +17,19 @@ const MyBanks = async () => {
       <div className="my-banks">
         <HeaderBox
           title="My Bank Accounts"
-          subtext="Effortlessly manage your banking activites."
+          subtext="Effortlessly manage your banking activities."
         />
 
         <div className="space-y-4">
           <h2 className="header-2">Your cards</h2>
           <div className="flex flex-wrap gap-6">
-            {accounts &&
-              accounts.data.map((a: Account) => (
-                <BankCard
-                  key={accounts.id}
-                  account={a}
-                  userName={loggedIn?.firstName}
-                />
-              ))}
+            {accounts && accounts.data.map((a: Account) => (
+              <BankCard
+                key={accounts.id}
+                account={a}
+                userName={loggedIn?.firstName}
+              />
+            ))}
           </div>
         </div>
       </div>
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
     }
 };
 
+module.exports = nextConfig
+
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
